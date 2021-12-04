f4f73fc596d74ad59b6515334cb14afff5d9fd0b
Canary233 <2096276316@qq.com>
2021-12-04 14:06:15 +0800

    防止苦力怕爆炸破坏地形

--- src/main/java/com/canary/Canary.java
+++ src/main/java/com/canary/Canary.java
@@ -7,11 +7,13 @@ public final class Canary extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new CreeperNotBoom(),this);
+        getLogger().info("§b插件已开启");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("插件已关闭");
     }
 }
---
+++ src/main/java/com/canary/CreeperNotBoom.java
@@ -0,0 +1,15 @@
+package com.canary;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+public class CreeperNotBoom implements Listener {
+    @EventHandler
+    public static void EEE(EntityExplodeEvent event){
+        if(event.getEntityType() == EntityType.CREEPER){
+            event.setCancelled(true);
+        }
+    }
+}
