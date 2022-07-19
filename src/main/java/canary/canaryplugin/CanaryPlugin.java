e79d5f905769a2df4af322afcb68fd80062689ec
Canary233 <2096276316@qq.com>
2022-07-19 15:50:13 +0800

    苦力怕爆炸不破坏地形

--- src/main/java/canary/canaryplugin/CanaryPlugin.java
+++ src/main/java/canary/canaryplugin/CanaryPlugin.java
@@ -6,6 +6,7 @@ public final class CanaryPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new CreeperNotExplode(),this);
         // Plugin startup logic
 
     }
