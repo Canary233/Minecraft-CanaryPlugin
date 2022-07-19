e3ac13edccd5a2ca75171d0ee53693e53f10ec67
Canary233 <2096276316@qq.com>
2022-07-19 15:20:43 +0800

    初始化

---
+++ src/main/java/canary/canaryplugin/CanaryPlugin.java
@@ -0,0 +1,17 @@
+package canary.canaryplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CanaryPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
